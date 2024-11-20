5b7c8ae30302ae8cc2684c9eb2655064fa5dcf12
Andre <Andre Graubner>
2024-11-21 02:11:12 +0800

    Remove bulk

--- train_segmentnt.py
+++ train_segmentnt.py
@@ -6,9 +6,7 @@ import random
 from transformers import AutoTokenizer, AutoModel
 import torch
 from einops import rearrange
-
-
-from model import OriNetNTSeg
+from torch import nn
 
 from data import get_split
 from torch.utils.data import DataLoader
@@ -25,6 +23,113 @@ from focal_loss import FocalLoss
 import sklearn
 import wandb
 
+import torch
+from torch.autograd import Variable
+from torch import nn
+import numpy as np
+import torch.nn.functional as F
+from pytorch_optimizer import Shampoo
+
+def jaccard_loss(preds, true, eps=1e-7):
+    """Computes the Jaccard loss, a.k.a the IoU loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the jaccard loss so we
+    return the negated jaccard loss.
+    Args:
+        true: a tensor of shape [B, H, W] or [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        jacc_loss: the Jaccard loss.
+    """
+    num_classes = preds.shape[1]
+    true_1_hot = F.one_hot(true)
+    true_1_hot = true_1_hot.type(preds.type())
+    intersection = torch.sum(preds * true_1_hot, dim=0)
+    cardinality = torch.sum(preds + true_1_hot, dim=0)
+    union = cardinality - intersection
+    jacc_loss = (intersection / (union + eps)).mean()
+    return (1 - jacc_loss)
+
+class FinalConv1D(nn.Module):
+    """
+    Final output block of the 1D-UNET.
+    """
+
+    def __init__(
+        self,
+        input_channels: int,
+        output_channels: int,
+        num_layers: int = 2,
+    ):
+        """
+        Args:
+            output_channels: number of output channels.
+            activation_fn: name of the activation function to use.
+                Should be one of "gelu",
+                "gelu-no-approx", "relu", "swish", "silu", "sin".
+            num_layers: number of convolution layers.
+            name: module name.
+        """
+        super().__init__()
+
+        self._first_layer = [nn.Conv1d(
+                in_channels=input_channels,
+                out_channels=output_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=1,
+                padding="same",
+            )]
+
+        self._next_layers = [
+            nn.Conv1d(
+                in_channels=output_channels,
+                out_channels=output_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=1,
+                padding="same",
+            )
+            for _ in range(num_layers-1)
+        ]
+        self.conv_layers = nn.ModuleList(self._first_layer + self._next_layers)
+
+        self._activation_fn = nn.SiLU()
+
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for i, conv_layer in enumerate(self.conv_layers):
+            x = conv_layer(x)
+            if i < len(self.conv_layers) - 1:
+                x = self._activation_fn(x)
+        return x
+
+class OriNetNTSeg(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = AutoModel.from_pretrained("InstaDeepAI/segment_nt_multi_species", trust_remote_code=True)
+        self.model.unet.final_block = FinalConv1D(
+            input_channels=1024,
+            output_channels=1024,
+            num_layers=2,
+        )
+        print(self.model.num_features)
+        self.model.num_features = 1
+        print(self.model.num_features)
+        self.model.fc = nn.Linear(in_features=1024, out_features=6 * 2)
+
+    def forward(self, tokens, attention_mask):
+
+        preds = self.model(
+                    tokens,
+                    attention_mask=attention_mask,
+                    output_hidden_states=True
+                ).logits
+        return preds
+
 wandb.init(
     project="ori-net",
 )
@@ -34,7 +139,7 @@ device = "cuda:0"
 # We use batch size 1 to not have to deal with bidirectional Mamba padding
 #dataset = OriDataset("./DoriC12.1/")
 #train, test = torch.utils.data.random_split(dataset, [0.9, 0.1], torch.Generator().manual_seed(42))
-train, test = get_split("plasmid_taxonomy_islands")
+train, test = get_split("plasmid_taxonomy_islands", max_length=12000)
 test_loader = DataLoader(test, batch_size=1, shuffle=False)
 
 # Initialize model
@@ -43,7 +148,7 @@ tokenizer = AutoTokenizer.from_pretrained("InstaDeepAI/segment_nt_multi_species"
 # Initialize some hyperparameters
 total_steps = 0
 grad_acc = 1
-n_epochs = 15
+n_epochs = 20
 warmup = 1000
 
 criterion = FocalLoss(gamma=2)
@@ -80,7 +185,7 @@ def evaluate():
                 targets = torch.zeros((1, preds.shape[1]), dtype=torch.long, device=device)
                 targets[0, labels[0,0]:labels[0,1] + 1] = 1 
                 
-                loss = criterion(
+                loss = jaccard_loss(
                     rearrange(preds, "b s c -> (b s) c"), 
                     rearrange(targets, "b s -> (b s)"),
                 )
@@ -123,8 +228,8 @@ for i in range(1):
     train_subset = torch.utils.data.Subset(train, indices)
     train_loader = DataLoader(train_subset, batch_size=1, shuffle=True)
 
-    #optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5, weight_decay=0.0)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-6) 
+    #optimizer = Shampoo(model.parameters(), lr=1e-5)
 
     scheduler = get_cosine_schedule_with_warmup(
         optimizer, 
@@ -157,7 +262,7 @@ for i in range(1):
                 targets = torch.zeros((1, preds.shape[1]), dtype=torch.long, device=device)
                 targets[0, labels[0,0]:labels[0,1] + 1] = 1 
 
-                loss = criterion(
+                loss = jaccard_loss(
                     rearrange(preds, "b s c -> (b s) c"), 
                     rearrange(targets, "b s -> (b s)"),
                 )
@@ -179,9 +284,8 @@ for i in range(1):
                 print(np.mean(losses))
                 losses = []
                 
-
             total_steps += 1
-            
+
         evaluate()
         
     torch.save(model.state_dict(), f"weights/model_{i}.pth")
