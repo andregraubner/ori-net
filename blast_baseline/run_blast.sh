e591c37251ba5451c50b06c3283d32e2896f56e5
André Graubner <32576384+andregraubner@users.noreply.github.com>
2024-12-07 14:18:30 +0800

    Create run_blast.sh

---
+++ blast_baseline/run_blast.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Create BLAST database from oris.fasta
+makeblastdb -in oris.fasta -dbtype nucl -out oris_db
+
+blastn -query test.fasta -db oris_db -outfmt "10 qseqid evalue length qstart qend" -out "results.csv"
